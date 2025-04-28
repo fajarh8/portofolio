f8e068605ced7ddfd1e774e499947bb36a7d0503
fajarh8 <fajar0heryan@gmail.com>
2025-04-28 08:19:29 +0700

    add landing page

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+// Untuk smooth scroll kecil
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
+    });
+  });
+});
